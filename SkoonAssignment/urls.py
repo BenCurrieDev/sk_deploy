495dd660efdd56e435b51c63f4a05ec7b6f7901d
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-02-02 22:15:23 +0000

    adjusted admin url to avoid bots

--- SkoonAssignment/urls.py
+++ SkoonAssignment/urls.py
@@ -21,7 +21,7 @@ from accounts import views as accounts_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('skoon-admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
