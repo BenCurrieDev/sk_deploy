e0d9137d172e6d32c17a33c4dcf1766203777ffa
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-02-02 22:13:13 +0000

    editted settings

--- SkoonAssignment/settings.py
+++ SkoonAssignment/settings.py
@@ -128,9 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = "/home/bencurriedev/sk_deploy/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
